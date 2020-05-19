1176d39af0fc9c10d92b30df64207cb944fca366
Prateek Chandan <prchan@microsoft.com>
2020-05-19 15:43:33 +0530

    Convert createElement to React component

--- ReactJS/TodoApp/app.js
+++ ReactJS/TodoApp/app.js
@@ -1,6 +1,13 @@
 
-var child1 = React.createElement('li', null, 'First Text Content');
-var child2 = React.createElement('li', null, 'Second Text Content');;
-var root = React.createElement('ul', { className: 'my-list' }, child1, child2);;
+'use strict';
 
-ReactDOM.render(root, document.getElementById('application'));;
+class List extends React.Component {
+  render() {
+    var child1 = React.createElement('li', null, 'First Text Content');
+    var child2 = React.createElement('li', null, 'Second Text Content');
+    var root = React.createElement('ul', { className: 'my-list' }, child1, child2);
+    return root;
+  }
+}
+
+ReactDOM.render(React.createElement(List), document.getElementById('application'));
