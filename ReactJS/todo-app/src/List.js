c9735cb22c23e698380d90f09265a2b0d37c6426
Prateek Chandan <prchan@microsoft.com>
2020-05-19 20:34:31 +0530

    Simple State example

--- ReactJS/todo-app/src/List.js
+++ ReactJS/todo-app/src/List.js
@@ -1,47 +1,22 @@
 import React from 'react';
-import {todoItemList} from "./TodoItem";
-import ListItem from './ListItem';
 
 class List extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            selectedId: -1
-        };
+        this.state = { value: 10 }; // initialize state in constructor always!
     }
 
-    render() {
+    render(props) {
         return (
-        <React.Fragment>
-            <ul>
-                {this.getAllListItems()}
-            </ul>
-            {this.state.description && <div>
-                {this.state.description}
+            <div>
+                <p>{this.state.value}</p> {/*Access state using this.state*/}
+                <button onClick={this.addOne}>Add</button>
             </div>
-            }
-        </React.Fragment>
         );
     }
-  
-    getAllListItems() {
-        let listItems = [];
-        todoItemList.forEach(item => {
-            listItems.push(
-            <ListItem 
-                item={item}
-                isSelected={item.id == this.state.selectedId}
-                onSelected={this.onItemSelected}
-            />);
-         });
-        return listItems;
-    }
 
-    onItemSelected = (item) => {
-        this.setState({
-            selectedId: item.id,
-            description: item.description
-        })
+    addOne = () => { // Arrow function to bind
+        this.setState({ value: this.state.value + 1 });
     }
 }
 
